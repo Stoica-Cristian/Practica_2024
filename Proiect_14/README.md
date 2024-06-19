291d57b9cf8795234b008a75946b72dec53d17ae
stoica.cristian <stoica.cristian@mta.ro>
2024-06-19 13:32:56 +0300

    Am adaugat functiile "check_for_modified_file" si "check_for_modified_files" care prelucreaza un snapshot si, pentru fiecare fisier, afiseaza daca acesta a fost modificat sau nu. Am modificat si imbunatatit funcitile create anterior.

--- Proiect_14/README.md
+++ Proiect_14/README.md
@@ -5,3 +5,7 @@ Am studiat subiectele propuse si l-am aprofundat pe cel ales.
 # 18.06.2024
 
 Am adaugat functiile "create_snapshot" si "compare_snapshots" cu optiunile si variantele corespunzatoare cerintelor si am construit logica scriptului printr un meniu interactiv
+
+# 19.06.2024
+
+Am adaugat functiile "check_for_modified_file" si "check_for_modified_files" care prelucreaza un snapshot si, pentru fiecare fisier, afiseaza daca acesta a fost modificat sau nu. Am modificat si imbunatatit funcitile create anterior.
--- Proiect_14/snapshot.sh
+++ Proiect_14/snapshot.sh
@@ -8,6 +8,7 @@ function create_snapshot() {
     read -p "Enter the snapshot name: " snapshot_name
 
     local timestamp=$(date +%Y-%m-%d_%H:%M:%S)
+    local current_timestamp_epoch=$(date +%s)
     local snapshot_path="${SNAPSHOT_DIR}/${snapshot_name}__${timestamp}.snapshot"
 
     mkdir -p "$SNAPSHOT_DIR"
@@ -19,13 +20,19 @@ function create_snapshot() {
 
     local exclude_str="${exclude_args[*]}"
 
-    sudo find / $exclude_str -xdev -type f -print > "$snapshot_path" 2>/dev/null
+    sudo find "$HOME/so" $exclude_str -xdev -type f -print > "$snapshot_path" 2>/dev/null
 
+    echo $current_timestamp_epoch >> "$snapshot_path"
     echo "Snapshot created: $snapshot_path"
 }
 
-function add_exclude_path() {
+function apend_to_excluded_paths() {
     local path_to_add="$1"
+
+    if [ ! -e "$path_to_add" ]
+    then
+        echo "[ apend_to_excluded_paths(path_to_add) ] - The path does not exist!"
+    fi
 
     for path in "${EXCLUDE_PATHS[@]}"; do
         if [[ "$path" == "$path_to_add" ]]; then
@@ -42,14 +49,24 @@ compare_snapshots() {
     local snapshot1="$1"
     local snapshot2="$2"
 
+    local timestamp1=$(tail -n 1 $snapshot1)
+    local timestamp2=$(tail -n 1 $snapshot2)
+
+    sed -i '$d' $snapshot1
+    sed -i '$d' $snapshot2
+
     sort -o $snapshot1 $snapshot1
     sort -o $snapshot2 $snapshot2
 
-    echo "Files created:"
+    echo -e "\nFiles created:"
     comm -13 "$snapshot1" "$snapshot2"
 
-    echo "Deleted files:"
+    echo -e "\nDeleted files:"
     comm -23 "$snapshot1" "$snapshot2"
+    echo
+
+    echo $timestamp1 >> $snapshot1
+    echo $timestamp2 >> $snapshot2
 }
 
 function compare_snapshots_specify_path() {
@@ -76,16 +93,77 @@ function compare_snapshots_remove_path() {
     touch "$SNAPSHOT_DIR/temp1"
     touch "$SNAPSHOT_DIR/temp2"
 
-    sed -E '\#($path_to_delete)#d' $snapshot1 > "$SNAPSHOT_DIR/temp1"
-    sed -E '\#($path_to_delete)#d' $snapshot2 > "$SNAPSHOT_DIR/temp2"
- 
+    sed "\#${path_to_delete}#d" $snapshot1 > "$SNAPSHOT_DIR/temp1"
+    sed "\#${path_to_delete}#d" $snapshot2 > "$SNAPSHOT_DIR/temp2"
+
     compare_snapshots "$SNAPSHOT_DIR/temp1" "$SNAPSHOT_DIR/temp2"
 
-    # rm "$SNAPSHOT_DIR/temp1" "$SNAPSHOT_DIR/temp2"
+    rm "$SNAPSHOT_DIR/temp1" "$SNAPSHOT_DIR/temp2"
 }
 
+function check_for_modified_files() {
+    local snapshot="$SNAPSHOT_DIR/$1"
 
-options=("Create snapshot" "Compare snapshots" "Exit")
+    echo
+    while read file_path
+    do
+        if [[ ! -f $file_path ]]
+        then
+            continue
+        fi
+
+        check_for_modified_file "$snapshot" "$file_path" "no"
+        local result=$(echo $?)
+
+        local filename=$(basename "$file_path")
+        local file_modification_date=$(stat -c %y $file_path | cut -d'.' -f1)
+
+        if [[ $result = '0' ]]
+        then
+            echo "The file $filename has not been modified."
+        elif [[ $result = '1' ]]
+        then
+            echo -e "\e[31mThe file $filename has been modified on ${file_modification_date}\e[0m"
+        fi
+
+    done < $snapshot
+    echo
+}
+
+function check_for_modified_file(){
+    local snapshot="$1"
+    local file_path="$2"
+    local only_one="$3"
+
+    # if [ ! -f "$filename" ]
+    # then
+    #     echo "File $filename nu există."
+    #     exit 1
+    # fi
+
+    local file_modification_date=$(stat -c %y $file_path)
+    local file_modification_epoch=$(date -d "$file_modification_date" +%s)
+    local snapshot_creation_time_epoch=$(tail -n 1 "$snapshot")
+
+    if [ "$only_one" = "only_one" ]
+    then
+        if [[ "$file_modification_epoch" -lt "$snapshot_creation_time_epoch" ]]
+        then
+            echo "The file has not been modified."
+        else
+            echo "The file has been modified."
+        fi
+    else
+        if [[ "$file_modification_epoch" -lt "$snapshot_creation_time_epoch" ]]
+        then
+            return 0
+        else
+            return 1
+        fi
+    fi
+}
+
+options=("Create snapshot" "Compare snapshots" "Check if the files has been modified" "Exit")
 PS3="Choose an option from the menu: " 
 select opt in "${options[@]}"
 do
@@ -103,11 +181,10 @@ do
                 read -p "Enter the path: " specific_path
                 compare_snapshots_specify_path "$SNAPSHOT_DIR/$snapshot1" "$SNAPSHOT_DIR/$snapshot2" "$specific_path"
             else
-                read -p "Do you want to remove a path from verifying? (y/n): " response
-                if [[ "$response" = 'y' ]]
+                read -p "Do you want to remove a path from verifying? (y/n): " remove_response
+                if [[ "$remove_response" = 'y' ]]
                 then
                     read -p "Enter the path to remove: " path_to_delete
-                    echo "$path_to_delete"
                     compare_snapshots_remove_path "$SNAPSHOT_DIR/$snapshot1" "$SNAPSHOT_DIR/$snapshot2" "$path_to_delete"
                 else
                     compare_snapshots "$SNAPSHOT_DIR/$snapshot1" "$SNAPSHOT_DIR/$snapshot2"
@@ -115,11 +192,19 @@ do
             fi
             ;;
         3)
-			echo "==EXIT=="
+            read -p "Specify the snapshot to check: " snapshot_files_check
+            read -p "Do you want to check for a specific file? (y/n): " file_response
+            if [[ "$file_response" = 'y' ]]
+            then
+                read -p "Enter the path to the file: " file_path
+                check_for_modified_file "$SNAPSHOT_DIR/$snapshot_files_check" "$file_path" "only_one"
+            else
+                check_for_modified_files "$snapshot_files_check"
+            fi
+            ;;
+        4)
+			echo -e "\n==EXIT==\n"
 			exit
 			;;
     esac
 done
-
-    # read -p "Enter the path to exclude: " exclude_path
-    # add_exclude_path "$exclude_path"
